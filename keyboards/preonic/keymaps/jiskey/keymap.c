7ad0651b4fa03446c3df77505e7bdf9b627d031d
papataku <sugoi.good@gmail.com>
2021-04-12 22:23:26 +0900

    ange keymaps

--- keyboards/preonic/keymaps/jiskey/keymap.c
+++ keyboards/preonic/keymaps/jiskey/keymap.c
@@ -65,9 +65,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
  * ,-----------------------------------------------------------------------------------.
- * |ESC   |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp  |
+ * |ESC   |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Del  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |Tab/F2|   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Enter|
+ * |Tab/F2|   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp|
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |Esc/F1|   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  | Enter|
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -89,8 +89,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_grid( \
-  KC_ESC,  KC_1,    SF_2,    KC_3,    KC_4,    KC_5,    SF_6,    SF_7,    SF_8,    SF_9,    SF_0,    KC_BSPC, \
-  FN2_TAB, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT,  \
+  KC_ESC,  KC_1,    SF_2,    KC_3,    KC_4,    KC_5,    SF_6,    SF_7,    SF_8,    SF_9,    SF_0,    KC_DEL, \
+  FN2_TAB, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,  \
   FN1_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    SF_SCLN, KC_ENT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   SF_QUOT,  \
   KC_LCTL, KC_LGUI, KC_LALT, ADJUST,  M_EMHL,  KC_SPC,  KC_SPC,  M_KHKR,  KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT  \
