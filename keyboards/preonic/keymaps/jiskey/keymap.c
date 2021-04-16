0f2ed9cd1f79ee486060015dd0186d6dcd1bbfb3
papataku <sugoi.good@gmail.com>
2021-04-16 10:00:14 +0900

    fix key

--- keyboards/preonic/keymaps/jiskey/keymap.c
+++ keyboards/preonic/keymaps/jiskey/keymap.c
@@ -77,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |ADJUST|   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  | Enter|
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |  Up  |' SFT |
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |  Up  | '    |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Ctrl | GUI  | Alt  | Fn2  |Eng/Lo|    Space    |Jpn/Ra| Fn1 /| Left | Down |Right |
  * `-----------------------------------------------------------------------------------'
@@ -98,7 +98,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GESC, KC_1,    SF_2,    KC_3,    KC_4,    KC_5,    SF_6,    SF_7,    SF_8,    SF_9,    SF_0,    SF_BSLS, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,  \
   ADJUST,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    SF_SCLN, KC_ENT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   SFT_QUOT,  \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   SF_QUOT,  \
   KC_LCTL, KC_LGUI, KC_LALT, FUNC2,   M_EMHL,  KC_SPC,  KC_SPC,  M_KHKR,  FN1_SLS, KC_LEFT, KC_DOWN, KC_RGHT  \
 ),
 
@@ -204,11 +204,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_FUNC2] = LAYOUT_preonic_grid( \
-  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6  , KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,  \
-  KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______, _______, KC_P7,   KC_P8,   KC_P9,    _______, KC_BSPC, \
-  _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_PAST, KC_PSLS, KC_P4,   KC_P5,   KC_P6,    _______, _______, \
-  _______, KC_F9,   KC_F10,  KC_F11 , KC_F12,  KC_PPLS, KC_PMNS, KC_P1,   KC_P2,   KC_P3,    KC_PEQL, _______, \
-  KC_LCTL, KC_LGUI, KC_LALT, _______, _______, _______, _______, KC_P0,   KC_PDOT, KC_PCMM, _______, _______ \
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6  , KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_DEL,  \
+  KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______, _______, KC_7,   KC_8,    KC_9,    _______, KC_BSPC, \
+  _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_PAST, KC_PSLS, KC_4,   KC_5,    KC_6,    _______, _______, \
+  _______, KC_F9,   KC_F10,  KC_F11 , KC_F12,  KC_PPLS, KC_PMNS, KC_1,   KC_2,    KC_3,    KC_PEQL, _______, \
+  KC_LCTL, KC_LGUI, KC_LALT, _______, _______, _______, _______, KC_0,   KC_DOT,  KC_COMM, _______, _______ \
 ),
 
 /* Adjust (Lower + Raise)
