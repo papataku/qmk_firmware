aa6f1bdfb2af6b77f8bf56d66fc1b9edfaec00b5
papataku <sugoi.good@gmail.com>
2024-08-21 22:46:46 +0900

    add keymap

---
+++ keyboards/sporewoh/minipeg48/keymaps/papataku/rules.mk
@@ -0,0 +1,10 @@
+#DEFERRED_EXEC_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
+#COMBO_ENABLE = yes
+
+#CONSOLE_ENABLE = yes
+
+CONSOLE_ENABLE = no         # Console for debug(+400)
+
+SRC +=  twpair_on_jis.c
+
